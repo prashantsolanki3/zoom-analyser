436a5032c0dea1172e3cb77ab8922787880c94ae
Prashant Solanki <solankisrp2@gmail.com>
2021-08-24 21:35:52 +1000

    Fix: impot ApplicationWrapper

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Grid, Typography } from '@material-ui/core';
 import { Image } from '../components';
+import ApplicationWrapper from '../bricks/ApplicationWrapper';
 
 const Component = () => {
     return (<Grid container justifyContent='flex-start' direction='column' alignItems='center'>

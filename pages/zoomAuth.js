477138d89d7172dce751380fae31410fc2b1bca5
Prashant Solanki <solankisrp2@gmail.com>
2021-08-25 00:10:08 +1000

    Remove 404 image, Added link for zoom auth

--- pages/zoomAuth.js
+++ pages/zoomAuth.js
@@ -19,12 +19,18 @@ const Component = () => {
     }, [])
 
     return (<Grid container justifyContent='flex-start' direction='column' alignItems='center'>
-        <Grid item xs={12} xl={2} lg={4} md={6} sm={8}>
-            <Image src={'https://cdn4.iconfinder.com/data/icons/robot-avatars-flat/60/028_-_404_bot-512.png'} />
-        </Grid>
         <Grid item xs={12} xl={12} lg={12} md={12} sm={12}>
             <Typography variant="body2" align="center" >
-            Start Zoom OAuth Process
+            <Link
+              style={{
+                pointer: 'cursor'
+              }}
+              color="textSecondary"
+              href={url}
+              variant="body2"
+            >
+              Start Zoom OAuth Process
+            </Link>
             </Typography>
         </Grid>
     </Grid>);

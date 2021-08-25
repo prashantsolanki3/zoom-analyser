90b88f532e5cedd9919352638291ad36e43e51a7
Prashant Solanki <solankisrp2@gmail.com>
2021-08-25 14:54:27 +1000

    New Dependency: next-iron-session Zoom login

--- pages/zoomAuth.js
+++ pages/zoomAuth.js
@@ -1,10 +1,15 @@
 import React, {useEffect, useState}  from 'react';
 import { Grid, Typography, Link } from '@material-ui/core';
-import { Image } from '../components';
+import { withIronSession } from "next-iron-session";
 import ApplicationWrapper from '../bricks/ApplicationWrapper';
 
-const Component = () => { 
+const Component = ({user, zoomUser, initialZoomCookie}) => { 
     const [url, setUrl] = useState(null)
+    const [thisZoomUser, setThisZoomUser] = useState(initialZoomCookie)
+    
+    console.log(zoomUser)
+    console.log(initialZoomCookie)
+
 
     const makeLink = () => {
 
@@ -12,7 +17,7 @@ const Component = () => {
 
         const thisUrl = new URL('https://zoom.us/oauth/authorize')
         thisUrl.searchParams.set('response_type', 'code')
-        thisUrl.searchParams.set('redirect_uri', process.env.NEXT_PUBLIC_REDIRECT_URL)
+        thisUrl.searchParams.set('redirect_uri',`${process.env.NEXT_PUBLIC_FRONTEND_HOSTNAME}/zoomAuth`)
         thisUrl.searchParams.set('client_id', process.env.NEXT_PUBLIC_ZOOM_CLIENT_ID)
         setUrl(thisUrl.href)
     }
@@ -38,6 +43,82 @@ const Component = () => {
         </Grid>
     </Grid>);
 }
+
+export const getServerSideProps = withIronSession(
+    async ({req, res}) => {
+        const user = req.session.get('user')
+        const thisUrl = new URL(req.url, `http://${req.headers.host}`)
+
+        const cookies = req.headers.cookie ? cookie.parse(req.headers.cookie) : null
+        const initialZoomCookie = cookies['ZOOMUSER'] ? cookies['ZOOMUSER'] : null
+        console.log(initialZoomCookie)
+        if (thisUrl.searchParams.get('code')){
+            const urlParam = thisUrl.searchParams.get('code')
+            console.log(urlParam)
+
+            const data = process.env.NEXT_PUBLIC_ZOOM_CLIENT + ':' + 
+                process.env.NEXT_PUBLIC_ZOOM_SECRET
+            const newData = Buffer.from(data, 'utf8')
+            const b64string = newData.toString('base64')
+
+            const zoomUrl = new URL('https://zoom.us/oauth/token')
+            zoomUrl.searchParams.set('grant_type', 'authorization_code')
+            zoomUrl.searchParams.set('code', urlParam)
+            zoomUrl.searchParams.set('redirect_uri', 
+            `${process.env.NEXT_PUBLIC_FRONTEND_HOSTNAME}/zoomAuth`
+            )
+            
+            try {
+                const options = {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': 'Basic ' + b64string
+                    }
+                }
+                const response = await fetch(zoomUrl.href, options)  
+                const json = await response.json()
+                console.log(json)
+
+                if (json.access_token){
+                    const preUser = await fetch('https://api.zoom.us/v2/users', {
+                        method: 'GET',
+                        headers: {
+                            'Authorization': 'Bearer ' + json.access_token
+                        }
+                    })
+                    const zoomUser = await preUser.json()
+                    console.log(zoomUser)
+                    if (!user){
+                        res.statusCode = 401
+                        return {props: {}}
+                    }
+                    return {
+                        props: {zoomUser, user}
+                    }
+                }
+            }
+            catch(e) {
+                console.log(e)
+            }        
+        }
+
+        if (!user){
+            res.statusCode = 401
+            return {props: {}}
+        }
+        return {
+            props: {user, initialZoomCookie}
+        }
+    },
+    {
+        cookieName: "ZOOMHELPERCOOKIE",
+        password: process.env.IRON_SESSION_SECRET,
+        cookieOptions: {
+            secure: process.env.NODE_ENV === 'production' ? true : false
+        }
+    }
+)
 
 const Export = () => {
     return <ApplicationWrapper>
